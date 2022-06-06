ca29bca647d390111487505b6a607e7fc368ec42
Techcable <Techcable@techcable.net>
2022-06-06 07:25:15 -0700

    Make the simple (C) interpreter have a switch version
    Needed for reference.

--- examples/simple_interpreter.c
+++ examples/simple_interpreter.c
@@ -5,6 +5,69 @@
 #include <stdlib.h>
 
 
+enum opcode {
+    OP_PUSH = 0,
+    OP_PUSH_NEG = 1,
+    OP_ADD = 2,
+    OP_PRINT = 3,
+    OP_POP = 4,
+    OP_RETURN_VALUE = 5,
+};
+
+int interpret_traditional(const uint8_t *ip) {
+    int entire_stack[16];
+    int *stack = &entire_stack[0];
+    int oparg, opcode;
+    #define DISPATCH() do { \
+            ip += 1; \
+            DECODE(); \
+            goto dispatchLoop; \
+        } while(false)
+    #define DECODE() do { \
+            opcode = *ip & 0xFF; \
+            oparg = (uint8_t) *ip >> 4; \
+        } while(false)
+    DECODE(); // initial
+    while (true) {
+        dispatchLoop:
+        switch (opcode) {
+            case OP_PUSH:
+                assert(oparg >= 0);
+                *stack++ = oparg;
+                DISPATCH();
+            case OP_PUSH_NEG:
+                assert(oparg >= 0);
+                *stack++ = -oparg;
+                DISPATCH();
+            case OP_ADD: {
+                int a = *--stack;
+                int b = *--stack;
+                *stack++ = a + b;
+                DISPATCH();
+            }
+            case OP_PRINT: {
+                int val = *--stack;
+                printf("%d\n", val);
+                DISPATCH();
+            }
+            case OP_POP: {
+                stack -= 1;
+                DISPATCH();
+            }
+            case OP_RETURN_VALUE: {
+                int val = *--stack;
+                return val;
+            }
+            default: {
+                fprintf(stderr, "Illegal insn: %d", (*ip & 0xFF));
+                abort();
+            }
+        }
+    }
+    #undef DISPATCH
+    #undef DECODE
+}
+
 int interpret(const uint8_t *ip) {
     int entire_stack[16];
     int *stack = &entire_stack[0];
@@ -64,15 +127,6 @@ int interpret(const uint8_t *ip) {
     }
 }
 
-enum opcode {
-    OP_PUSH = 0,
-    OP_PUSH_NEG = 1,
-    OP_ADD = 2,
-    OP_PRINT = 3,
-    OP_POP = 4,
-    OP_RETURN_VALUE = 5,
-};
-
 #define BC(op, oparg) ((uint8_t) (((enum opcode) op) | (oparg << 4)))
 const uint8_t SAMPLE_BYTECODE[6] = {
     BC(OP_PUSH, 2),
@@ -86,5 +140,7 @@ const uint8_t SAMPLE_BYTECODE[6] = {
 
 int main(int argc, char** argv) {
     int res = interpret(&SAMPLE_BYTECODE[0]);
+    int res2 = interpret_traditional(&SAMPLE_BYTECODE[0]);
+
     return res != 0;
 }
